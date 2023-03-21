975cac5dc4e144f44ec294ecede67857a54263e0
Simon <simonbengan8@gmail.com>
2023-03-21 13:58:28 +0100

    test that add todo can be created.

--- 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -1,14 +1,32 @@
 import { describe, it, expect } from 'vitest'
 import * as TodoAPI from '../services/TodoAPI'
+import { CreateTodoData } from '../types/Todo'
+
+const newTodo: CreateTodoData = {
+    title: "Test todo",
+    completed: false,
+}
 
 describe('TodoAPI', () => {
 
-    it.todo('should return a list')
+    it('should return a list', async () => {
+        const todos = await TodoAPI.getTodos()
 
-    it.todo('should create a new todo')
+        expect(Array.isArray(todos)).toBe(true)
+    })
+
+    it('should create a new todo', async () => {
+        const result = await TodoAPI.createTodo(newTodo)
+
+        expect(result).toMatchObject({
+            id: expect.any(Number),
+            title: newTodo.title,
+            completed: newTodo.completed
+        })
+    })
 
     it.todo('should create and then get the todo')
-    
+
     it.todo('should create and then find the todo amoung all todos')
     
     it.todo('should create and then update the todo')
