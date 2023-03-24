b2e465a56dcf295142c407c6a0d476842fdbc187
Simon <simonbengan8@gmail.com>
2023-03-24 11:17:23 +0100

    Test that the features on the home page are correct.

--- 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -1,9 +1,25 @@
 describe('Home Page', () => {
-    it('h1 contains the correct text', () => {
-      cy.visit('http://localhost:3000')
 
-      cy.get('h1')
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+    it('h1 contains the correct text', () => {
+      cy.get('[data-test="hero-heading"]')
         .should('exist')
         .contains('Testing Next.js Applications with Cypress')
+    })
+
+    it('the features on the home page are correct', () => {
+      cy.get('dt')
+        .eq(0)
+        .contains('4 Courses')
+
+        cy.get('dt')
+        .eq(1)
+        .contains('25+ Lessons')
+
+        cy.get('dt')
+        .eq(2)
+        .contains('Free and Open Source')
     })
 })
