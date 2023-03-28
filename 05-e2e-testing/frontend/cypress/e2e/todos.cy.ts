8dcccd660e5365228ff8ca8ed76d775a2a852104
Simon <simonbengan8@gmail.com>
2023-03-28 11:04:31 +0200

    created 05-e2e-testing.

---
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('should be able to visit the page', () => {
+    cy.visit('/')
+  })
+})
---
+++ 05-e2e-testing/frontend/src/services/TodoAPI.ts
@@ -0,0 +1,73 @@
+import axios from 'axios'
+import { Todo, CreateTodoData, UpdateTodoData } from '../types/Todo'
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+/**
+ * Generic get request
+ */
+export const get = async <T>(endpoint: string) => {
+	const res = await axios.get<T>(BASE_URL + endpoint)
+	return res.data
+}
+
+/**
+ * Generic post request
+ */
+export const post = async <T>(endpoint: string, data: any) => {
+	const res = await axios.post<T>(BASE_URL + endpoint, data)
+	return res.data
+}
+
+/**
+ * Generic patch request
+ */
+export const patch = async <T>(endpoint: string, data: any) => {
+	const res = await axios.patch<T>(BASE_URL + endpoint, data)
+	return res.data
+}
+
+/**
+ * Generic delete request
+ */
+export const del = async (endpoint: string) => {
+	const res = await axios.delete(BASE_URL + endpoint)
+	return res.data
+}
+
+
+/**
+ * Get all todos
+ */
+export const getTodos = async () => {
+	return get<Todo[]>(`/todos`)
+}
+
+/**
+ * Get a single todo
+ */
+
+export const getTodo = (id: number) => {
+	return get<Todo>(`/todos/${id}`)
+}
+
+/**
+ * Create a new todo
+ */
+export const createTodo = (todo: CreateTodoData) => {
+	return post<Todo>(`/todos`, todo)
+}
+
+/**
+ * Update a todo
+ */
+export const updateTodo = (id: number, todo: UpdateTodoData) => {
+	return patch<Todo>(`/todos/${id}`, todo)
+}
+
+/**
+ * Delete a todo
+ */
+export const deleteTodo = (id: number) => {
+	return del(`/todos/${id}`)
+}
